0ff6b8d48842535ac596b5ed5f47b6c79ed19f64
MauroKeyes <keyesm19601@live.bucks.edu>
2016-06-06 23:08:33 -0400

    Labe 1 complete

---
+++ Produce.sql
@@ -0,0 +1,10 @@
+	CREATE TABLE Produce
+    (
+
+    ProduceId int PRIMARY KEY AUTO_INCREMENT, 
+
+    Name varchar(35), 
+
+    ProduceType varchar(10),
+    Color  varchar(35)
+    );
---
+++ grabber.py
@@ -0,0 +1,17 @@
+#LAB1 Python starter code #Based upon manual at http://mysql-python.sourceforge.net/MySQLdb.html
+#imports go here #import MySQLdb 
+import _mysql
+
+#code goes hereshould picked root
+
+
+
+db = _mysql.connect(host="localhost",user="root",passwd="<Obfuscated>",db="MauroLab1") 
+db.query("""SELECT * FROM PrOdUcE;""") 
+r = db.store_result() 
+nR = r.num_rows() 
+while(nR > 0):
+	print(r.fetch_row()) 
+	nR = nR - 1
+db.close()
+     
